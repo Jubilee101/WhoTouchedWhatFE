cd127f1de4bba6579251537a1ebce95fda4b5dc4
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-07-18 21:05:54 -0400

    add a pie

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,10 @@
 import { message } from "antd";
 import React, { useState } from "react";
-import { Form, Button, Input } from "antd";
+import { Form, Button, Input, Row, Col } from "antd";
 import { parse } from "../utils";
 import Tree from "./Tree"
+import PieChart from "./PieChart";
+import "./HomePage.css"
 class HomePage extends React.Component {
     render() {
         return <ParseDirectory/>
@@ -12,13 +14,18 @@ class HomePage extends React.Component {
 function ParseDirectory() {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
+    const [authors, setAuthors] = useState([]);
+    const [pieVisibility, setPieVisibility] = useState(false);
 
     const getTree = async (query) => {
         setLoading(true);
         try {
             const resp = await parse(query);
-            setData(oldData => [...resp.children]
-            );
+            console.log(resp);
+            setData(oldData => [...resp.children]);
+            setAuthors(oldAuthors =>[...resp.authors])
+            setPieVisibility(true);
+            
         } catch (error) {
             message.error(error.message);
         } finally {
@@ -27,30 +34,37 @@ function ParseDirectory() {
     }
     return (
         <>
-        <div style={{ width: 500, margin: "20px left" }}>
-        <Form onFinish={getTree} layout="verticle">
-            <Form.Item
-                label="Repository Directory"
-                name="file_path"
-                rules={[{ required: true }]}
-            >
-            <Input
-                disabled={loading}
-                placeholder="Repo Path"
-            />
-        </Form.Item>
-        <Form.Item>
-        <Button
-            loading={loading}
-            type="primary"
-            htmlType="submit"
-        >
-            Parse
-        </Button>
-        </Form.Item>
-        </Form>
-        </div>
-        <Tree data={data} />
+        <Row>
+            <Col span={12} className='left-side'>
+                <Form onFinish={getTree} layout="verticle">
+                    <Form.Item
+                        label="Repository Directory"
+                        name="file_path"
+                        rules={[{ required: true }]}
+                    >
+                    <Input
+                        disabled={loading}
+                        size="middle"
+                        style={{ width: '60%' }}
+                        placeholder="Repo Path"
+                    />
+                </Form.Item>
+                <Form.Item>
+                <Button
+                    loading={loading}
+                    type="primary"
+                    htmlType="submit"
+                >
+                    Parse
+                </Button>
+                </Form.Item>
+                </Form>
+                <Tree data={data} />
+        </Col>
+        <Col span={12} className="right-side">
+            {pieVisibility && <PieChart data = {authors}/>}
+        </Col>
+        </Row>
         </>
     )
 
